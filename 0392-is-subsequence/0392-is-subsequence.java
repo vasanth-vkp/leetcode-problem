8dceca055fc894b313932f9c10c2a28ffe67e187
vasanth-vkp <vasanthvkp2007@gmail.com>
2026-07-21 23:13:19 +0530

    Time: 7 ms (2.02%), Space: 44 MB (11.36%) - LeetHub

---
+++ 0392-is-subsequence/0392-is-subsequence.java
@@ -0,0 +1,24 @@
+class Solution {
+    public boolean isSubsequence(String s, String t) {
+        int n=s.length();
+        int m=t.length();
+        int[][] dp=new int[n+1][m+1];
+        for(int i=0;i<=n;i++){
+            for(int j=0;j<=m;j++){
+                if(i==0 || j==0){
+                    dp[i][j]=0;
+                }
+                else if(s.charAt(i-1)==t.charAt(j-1)){
+                    dp[i][j]=dp[i-1][j-1]+1;
+                }
+                else{
+                    dp[i][j]=Math.max(dp[i][j-1],dp[i-1][j]);
+                }
+            }
+        }
+        if(dp[n][m]==n){
+            return true;
+        }
+        return false;
+    }
+}
