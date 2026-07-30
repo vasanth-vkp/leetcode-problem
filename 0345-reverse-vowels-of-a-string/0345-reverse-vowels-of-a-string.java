6f0d56cef5d975a6a8a9ba40954044075b097f27
vasanth-vkp <vasanthvkp2007@gmail.com>
2026-07-30 12:24:45 +0530

    Time: 2 ms (99.29%), Space: 46.1 MB (94.25%) - LeetHub

--- 0345-reverse-vowels-of-a-string/0345-reverse-vowels-of-a-string.java
+++ 0345-reverse-vowels-of-a-string/0345-reverse-vowels-of-a-string.java
@@ -2,43 +2,36 @@ class Solution {
     public String reverseVowels(String s) {
         int n = s.length();
         char[] arr = s.toCharArray();
-
         int left = 0;
         int right = n - 1;
-
         while (left < right) {
-
             boolean leftVowel =
-                    arr[left] == 'a' ||
-                    arr[left] == 'e' ||
-                    arr[left] == 'i' ||
-                    arr[left] == 'o' ||
-                    arr[left] == 'u' ||
-                    arr[left] == 'A' ||
-                    arr[left] == 'E' ||
-                    arr[left] == 'I' ||
-                    arr[left] == 'O' ||
-                    arr[left] == 'U';
+                    arr[left] =='a' ||
+                    arr[left] =='e' ||
+                    arr[left] =='i' ||
+                    arr[left] =='o' ||
+                    arr[left] =='u' ||
+                    arr[left] =='A' ||
+                    arr[left] =='E' ||
+                    arr[left] =='I' ||
+                    arr[left] =='O' ||
+                    arr[left] =='U';
 
             boolean rightVowel =
-                    arr[right] == 'a' ||
-                    arr[right] == 'e' ||
-                    arr[right] == 'i' ||
-                    arr[right] == 'o' ||
-                    arr[right] == 'u' ||
-                    arr[right] == 'A' ||
-                    arr[right] == 'E' ||
-                    arr[right] == 'I' ||
-                    arr[right] == 'O' ||
-                    arr[right] == 'U';
-
+                    arr[right] =='a' ||
+                    arr[right] =='e' ||
+                    arr[right] =='i' ||
+                    arr[right] =='o' ||
+                    arr[right] =='u' ||
+                    arr[right] =='A' ||
+                    arr[right] =='E' ||
+                    arr[right] =='I' ||
+                    arr[right] =='O' ||
+                    arr[right] =='U';
             if (leftVowel && rightVowel) {
-
                 char temp = arr[left];
-
                 arr[left] = arr[right];  
                 arr[right] = temp;        
-
                 left++;                  
                 right--;                  
             }
